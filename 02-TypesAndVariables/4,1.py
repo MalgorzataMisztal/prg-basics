0f1d420324d5ccc370df808540877cbeb93f4e58
MalgorzataMisztal <242495@student.uek.krakow.pl>
2025-10-28 21:42:49 +0100

    i did 4

---
+++ 02-TypesAndVariables/4,1.py
@@ -0,0 +1,7 @@
+###
+# Printing student's personal data
+#
+name = "Adam"
+print(f"My name is {name}. \n"
+      "I am 7 years old, and my height is 140 cm. \n"
+      "In 6 years I will be 13 years old.")
